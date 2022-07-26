2089dba27a5e3405118a0b5ffab5700732377c4e
hsohans <hso@innogrid.com>
2022-07-26 07:47:55 +0000

    - Remove unused package (filebeat, java8, logstash)

--- create-release.sh
+++ create-release.sh
@@ -8,9 +8,9 @@ if [ -d ./dev_releases ]; then
   echo "delete dev_releases"
   rm -rf dev_releases
 fi
-if [ -f ./monitoring-agent-release-5.8.0.tgz ]; then
-  echo "delete monitoring-agent-release-5.8.0.tgz"
-  rm -rf monitoring-agent-release-5.8.0.tgz
+if [ -f ./monitoring-agent-release-5.8.1.tgz ]; then
+  echo "delete monitoring-agent-release-5.8.1.tgz"
+  rm -rf monitoring-agent-release-5.8.1.tgz
 fi
 
 #if [ -d ./blobs ]; then
@@ -19,4 +19,4 @@ fi
 #fi
 #
 
-bosh create-release --force --tarball monitoring-agent-release-5.8.0.tgz --name monitoring-agent-release --version 5.8.0
+bosh create-release --force --tarball monitoring-agent-release-5.8.1.tgz --name monitoring-agent-release --version 5.8.1
